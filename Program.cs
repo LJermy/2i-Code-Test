665e51c32de17dc67c04821b687a2d377dde5e3d
LJermy <89399384+LJermy@users.noreply.github.com>
2022-07-14 10:35:53 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _2i_Code_Test
+{
+    class Program
+    {
+        static int multiplier;
+        static int upperLimit;
+        static int loopCount = 3;
+        static void Main(string[] args)
+        {
+            getInputs();
+
+            for (int i = 0; i <= loopCount-1; i++)                               //Loops 3 times calling the iterate method. Could be edited to get user input to iterate any number of times requested
+            {
+                iterate(i, i + 1);
+            }
+                
+        }
+
+        public static void getInputs()
+        {
+            Console.WriteLine("Enter the multiplier");                  //Gets user input for multiplier and converts it to an int
+            multiplier = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Upper Limit");
+            upperLimit = Convert.ToInt32(Console.ReadLine());           //Gets user input for upperLimit and converts it to an int
+        }
+
+        public static void iterate(int offset, int modifier)
+        {
+            int increment = multiplier + offset;                    //Sets the number that the total will increase by each loop
+            int i;
+
+            for (i = increment; i < upperLimit * modifier; i = i + increment)  //iterates starting from the calculated number and iterates through and stops before going over the upper limit
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(i + "\n");                                       //Prints the last value of i calculated, will be equal to or over the upper limit and wont itterate again
+        }
+    }
+}
